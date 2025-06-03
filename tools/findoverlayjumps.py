e6b78143e33822b798dcfbb6b7712eb94210c64f
Isma <122747777+CacaBueno64@users.noreply.github.com>
2025-06-04 00:47:39 +0200

    Update findoverlayjumps.py

--- tools/findoverlayjumps.py
+++ tools/findoverlayjumps.py
@@ -1,26 +1,26 @@
 import os
 
 src_dir = "./ie3/asm"
-current_file = "overlay1.s"
+current_file = "overlay15.s"
 addresses = {}
 
 def makedict():
-    for filename in os.listdir(src_dir):
-        if "overlay" in filename and ".s" in filename and current_file not in filename and "_" not in filename:
-            src_file = os.path.join(src_dir, filename)
-            if os.path.isfile(src_file):
-                with open(src_file, "rt") as f:
-                    txt = f.read()
-                    for line in txt.splitlines():
-                        if line.startswith("FUN") and ":" in line:
-                            ovname = filename.replace(".s", "")
-                            ovname = ovname.replace("erlay", "")
-                            idx = line.find("_0")
-                            address = line[idx+1:idx+9].upper()
-                            if address not in addresses:
-                                addresses[address] = []
-                            addresses[address].append(ovname)
-
+    ovname = current_file.replace("erlay", "").replace(".s", "")
+    with open("./ie3/asm/global.inc", "rt") as file:
+        txt = file.read()
+        for line in txt.splitlines():
+            if ovname not in line and "FUN_02" not in line:
+                line = line.strip(" \n")
+                funname = line.replace(".public", "")
+                idx = funname.find("_0")
+                funaddr = funname[idx+1:idx+9]
+                if funaddr.upper() not in addresses:
+                    addresses[funaddr.upper()] = []
+                addresses[funaddr.upper()].append(funname)
+def findovname(funname):
+    idx1 = funname.find("ov")
+    idx2 = funname.find("_0")
+    return funname[idx1:idx2]
 def replacelabs():
     with open(f"./ie3/asm/{current_file}", "rt") as file, open("./ie3/asm/atest.s", "wt") as out:
         txt = file.read()
@@ -30,11 +30,13 @@ def replacelabs():
                 idx = line.find("FUN_0")
                 funaddr = line[idx+4:idx+4+8]
                 if funaddr in addresses:
-                    ovname = addresses[funaddr][0]
-                    funname = f"FUN_{ovname}_{funaddr.lower()}"
+                    funname: str = addresses[funaddr][0]
+                    idx1 = funname.find("ov")
+                    idx2 = funname.find("_0")
+                    ovname = funname[idx1:idx2]
                     multiple = ""
                     if len(addresses[funaddr]) > 1:
-                        multiple = " ; may be " + " ".join(addresses[funaddr][1:])
+                        multiple = " ; may be " + " ".join([findovname(j) for j in addresses[funaddr][1:]])
                     line = line.replace(f"FUN_{funaddr}", funname + multiple)
             output += line + "\n"
         out.write(output)
