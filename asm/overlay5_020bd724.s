d4c5972c1bea502eddba84d5bae11009e82cbd7b
Isma <122747777+CacaBueno64@users.noreply.github.com>
2025-06-03 23:57:57 +0200

    Delete asm/overlay5_020bd724.s

--- asm/overlay5_020bd724.s
+++
@@ -1,9 +0,0 @@
-
-
-
-	arm_func_start FUN_ov5_020bd724
-FUN_ov5_020bd724: ; 0x020BD724
-	ldr r12, _020BD72C ; =FUN_ov5_020bd538
-	bx r12
-_020BD72C: .word FUN_ov5_020bd538
-	arm_func_end FUN_ov5_020bd724
