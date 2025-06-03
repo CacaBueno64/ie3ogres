7b5f01c50f1ccac3c9531e1f3892a88d3db160f5
Isma <122747777+CacaBueno64@users.noreply.github.com>
2025-06-03 13:42:58 +0200

    Delete asm/overlay0_020d1ca8.s

--- asm/overlay0_020d1ca8.s
+++
@@ -1,9 +0,0 @@
-
-
-    arm_func_start FUN_ov0_020D1CA8
-FUN_ov0_020D1CA8:
-    ldr r12, _020D1CB0 ; =FUN_02006ed0
-    bx r12
-_020D1CB0: .word FUN_02006ed0
-    arm_func_start FUN_ov0_020D1CA8
-    ; 0x020D1CB4
