bfbb90b7bba88b5b4d209ce0801f38fac3935a75
Isma <122747777+CacaBueno64@users.noreply.github.com>
2025-06-04 17:19:59 +0200

    Update findoverlayjumps.py

--- tools/findoverlayjumps.py
+++ tools/findoverlayjumps.py
@@ -1,7 +1,7 @@
 import os
 
 src_dir = "./ie3/asm"
-current_file = "overlay15.s"
+current_file = "overlay17.s"
 addresses = {}
 
 def makedict():
@@ -29,14 +29,14 @@ def replacelabs():
             if "FUN_0" in line or "=FUN_0" in line:
                 idx = line.find("FUN_0")
                 funaddr = line[idx+4:idx+4+8]
-                if funaddr in addresses:
-                    funname: str = addresses[funaddr][0]
+                if funaddr in addresses or funaddr.upper() in addresses:
+                    funname: str = addresses[funaddr.upper()][0]
                     idx1 = funname.find("ov")
                     idx2 = funname.find("_0")
                     ovname = funname[idx1:idx2]
                     multiple = ""
-                    if len(addresses[funaddr]) > 1:
-                        multiple = " ; may be " + " ".join([findovname(j) for j in addresses[funaddr][1:]])
+                    if len(addresses[funaddr.upper()]) > 1:
+                        multiple = " ; may be " + " ".join([findovname(j) for j in addresses[funaddr.upper()][1:]])
                     line = line.replace(f"FUN_{funaddr}", funname + multiple)
             output += line + "\n"
         out.write(output)
