5eb445ad479232e5e23dbb105559d521a5e8a5d3
Isma <122747777+CacaBueno64@users.noreply.github.com>
2025-06-22 22:28:39 +0200

    Delete comp.py

--- comp.py
+++
@@ -1,9 +0,0 @@
-from ndspy import codeCompression
-
-curfile = "overlay_0001"
-
-with open(f"../dslazy/NDS_UNPACK/overlay/{curfile}.bin", "rb") as file, \
-    open(f"../dslazy/NDS_UNPACK/overlay/{curfile}.decomp", "wb") as out:
-        data = file.read()
-        decomp = codeCompression.decompress(data)
-        out.write(decomp)
