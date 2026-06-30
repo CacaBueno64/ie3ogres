ad7beac843e0268ecfe4fc36967980a0855ab3db
Isma <122747777+CacaBueno64@users.noreply.github.com>
2026-06-30 21:55:02 +0200

    Update C3DDevice.cpp

--- src/C3DDevice.cpp
+++ src/C3DDevice.cpp
@@ -26,15 +26,15 @@ BOOL C3DDevice::init()
     G3X_AlphaTest(FALSE, 0);
     G3X_AlphaBlend(TRUE);
     this->setClearColor(GX_RGB(0, 0, 0), 0, FALSE);
-    this->setEdgeMarking(1);
-    this->setEdgeColor(0);
-    this->setAntiAliasing(1);
+    this->setEdgeMarking(TRUE);
+    this->setEdgeColor(GX_RGB(0, 0, 0));
+    this->setAntiAliasing(TRUE);
     for (int i = 0; i < 8; ++i) {
         fog_table[i] = (16 * i) | ((16 * i + 4) << 8) | ((16 * i + 8) << 16) | ((16 * i + 12) << 24);
     }
     this->setFog(FALSE);
     this->setFog(TRUE, GX_FOGSLOPE_0x2000, 0x5800);
-    this->setFogColor(0, 0);
+    this->setFogColor(GX_RGB(0, 0, 0), 0);
     this->setFogTable(fog_table);
     for (int i = 0; i < 0x20; ++i) {
         toon_table[i] = GX_RGB(i, i, i);
@@ -43,7 +43,7 @@ BOOL C3DDevice::init()
     G3X_SetToonTable(toon_table);
     NNS_G3dGeViewPort(0, 0, GX_LCD_SIZE_X - 1, GX_LCD_SIZE_Y - 1);
     Shadow::Initialize(8);
-    NNS_G3dGeUseFastDma(0);
+    NNS_G3dGeUseFastDma(FALSE);
     return TRUE;
 }
 
@@ -58,21 +58,13 @@ void C3DDevice::setClearColor(GXRgb color, u8 alpha, BOOL fog)
 void C3DDevice::setAntiAliasing(BOOL enable)
 {
     this->antiAliasing = enable;
-    if (enable) {
-        G3X_AntiAlias(TRUE);
-        return;
-    }
-    G3X_AntiAlias(FALSE);
+    G3X_AntiAlias(enable);
 }
 
 void C3DDevice::setEdgeMarking(BOOL enable)
 {
     this->edgeMarking = enable;
-    if (enable) {
-        G3X_EdgeMarking(TRUE);
-        return;
-    }
-    G3X_EdgeMarking(FALSE);
+    G3X_EdgeMarking(enable);
 }
 
 void C3DDevice::setEdgeColor(GXRgb edge_color)
@@ -86,7 +78,7 @@ void C3DDevice::setEdgeColor(GXRgb edge_color)
 void C3DDevice::setFog(BOOL enable)
 {
     this->fog.enable = enable;
-    G3X_SetFog(this->fog.enable, (GXFogBlend)(this->fog).blend, (GXFogSlope)(this->fog).slope, (int)(this->fog).offset);
+    G3X_SetFog(this->fog.enable, static_cast<GXFogBlend>(this->fog.blend), static_cast<GXFogSlope>(this->fog.slope), this->fog.offset);
 }
 
 void C3DDevice::setFog(BOOL color, GXFogSlope slope, u16 offset)
@@ -102,7 +94,7 @@ void C3DDevice::setFog(BOOL color, GXFogSlope slope, u16 offset)
     }
     this->fog.offset = offset;
     if (this->fog.enable) {
-        this->setFog(1);
+        this->setFog(TRUE);
     }
 }
 
@@ -115,9 +107,10 @@ void C3DDevice::setFogColor(GXRgb color, u8 alpha)
 
 void C3DDevice::setFogTable(u32 *fog_table)
 {
-    if (!fog_table) return;
-    MIi_CpuCopyFast(fog_table, this->fog.table, sizeof(this->fog.table));
-    G3X_SetFogTable(fog_table);
+    if (fog_table) {
+        MI_CpuCopyFast(fog_table, this->fog.table, sizeof(this->fog.table));
+        G3X_SetFogTable(fog_table);
+    }
 }
 
 void C3DDevice::reset()
